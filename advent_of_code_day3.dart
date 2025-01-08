1775abc56b33453584e53b3d4e355217bf2243f8
Suchar <39964811+SucharMistrz@users.noreply.github.com>
2025-01-08 14:03:03 +0100

    Initial commit for Day 3 branch

---
+++ advent_of_code_day3.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  File inputFile = File('input_advent_of_code_day3.txt');
+  String fileContent = await inputFile.readAsString();
+
+
+
+  print('');
+}
