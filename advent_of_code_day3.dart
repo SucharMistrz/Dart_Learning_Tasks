ea36751900ce3cc5a26b40531ef5a8adc4cbc144
Suchar <39964811+SucharMistrz@users.noreply.github.com>
2025-01-09 09:47:50 +0100

    Updated to include the answer to Day 3, Part 2.

--- advent_of_code_day3.dart
+++ advent_of_code_day3.dart
@@ -2,24 +2,37 @@ import 'dart:core';
 import 'dart:io';
 
 void main(List<String> arguments) async {
-
   File inputFile = File('input_advent_of_code_day3.txt');
   String fileContent = await inputFile.readAsString();
 
-  int totalValue = 0;
-  final RegExp findMul = RegExp(r'mul\(\d+,\d+\)');
+  bool mulEnabled = true;
+  int totalMulValue = 0;
+
+  /*Find one of the following statements:
+  Group 1: do()
+  Group 2: don't()
+  Group 3: mul(<number>,<number>)
+   */
+  final RegExp findBasicInfo = RegExp(r"(do\(\))|(don't\(\))|(mul\(\d+,\d+\))");
+
+  //Find just the numbers within the Mul statement (Group 3 of the previous RegExp)
+  //in order to use them to calculate the final value
   final RegExp findNumbers = RegExp(r'\d+');
 
-  for (final RegExpMatch expressionsFound in findMul.allMatches(fileContent))
-  {
-    String? s = expressionsFound.group(0);
-    List<int> numbers = <int>[];
+  for (final RegExpMatch basicInfo in findBasicInfo.allMatches(fileContent)) {
+    if (basicInfo.group(1) != null) {
+      mulEnabled = true;
+    } else if (basicInfo.group(2) != null) {
+      mulEnabled = false;
+    } else if (mulEnabled == true && basicInfo.group(3) != null) {
+      List<int> mulArgs = <int>[];
 
-    for (final RegExpMatch numbersFound in findNumbers.allMatches(s!))
-    {
-      numbers.add(int.parse(numbersFound.group(0)!));
+      for (final RegExpMatch numbersFound
+          in findNumbers.allMatches(basicInfo.group(3)!)) {
+        mulArgs.add(int.parse(numbersFound.group(0)!));
+      }
+      totalMulValue += mulArgs[0] * mulArgs[1];
     }
-    totalValue += numbers[0] * numbers[1];
   }
-  print(totalValue);
+  print(totalMulValue);
 }
