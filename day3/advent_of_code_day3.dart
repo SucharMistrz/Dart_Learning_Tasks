aab5b4eb93280b43175ed423b541dc466aded055
Suchar <39964811+SucharMistrz@users.noreply.github.com>
2025-01-09 10:10:22 +0100

    Moved folders in Day 3 branch to correspond to the new standard in Master

--- day3/advent_of_code_day3.dart
+++ day3/advent_of_code_day3.dart
@@ -2,7 +2,7 @@ import 'dart:core';
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  File inputFile = File('input_advent_of_code_day3.txt');
+  File inputFile = File('input.txt');
   String fileContent = await inputFile.readAsString();
 
   bool mulEnabled = true;
