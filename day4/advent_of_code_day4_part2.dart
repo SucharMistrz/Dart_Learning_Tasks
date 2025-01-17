310ad88a177d3bb443ae46ec307748cbb816b960
Suchar <39964811+SucharMistrz@users.noreply.github.com>
2025-01-17 08:32:33 +0100

    Added comments for Day 4 Part 2

--- day4/advent_of_code_day4_part2.dart
+++ day4/advent_of_code_day4_part2.dart
@@ -5,21 +5,24 @@ void main(List<String> arguments) async {
   File inputFile = File('input.txt');
   List<String> fileContent = await inputFile.readAsLines();
 
-  int totalNumberOfFoundWords = 0;
+  int totalNumberOfFoundShapes = 0;
   String keyword = 'MAS';
 
+  //Unlike Day 4 Part 1, here we do not allow just any random input
   if (keyword.length != 3) {
     throw Error();
   }
 
   for (int row = 1; row < fileContent.length - 1; row++) {
     for (int column = 1; column < fileContent[row].length - 1; column++) {
+      //Find the middle of the X shape
       if (fileContent[row][column] != keyword[1]) {
         continue;
       }
 
       int localFoundWords = 0;
 
+      //Once the middle has been found, iterate the X shape in all 4 directions
       if ((row > 0) && (column > 0)) {
         //Up Left
         if (hasFoundKeyword(fileContent, keyword, column + 1, row + 1,
@@ -51,12 +54,13 @@ void main(List<String> arguments) async {
         }
       }
 
+      //The input word has to be found in at least two directions to confirm they form an X shape
       if (localFoundWords == 2) {
-        totalNumberOfFoundWords++;
+        totalNumberOfFoundShapes++;
       }
     }
   }
-  print(totalNumberOfFoundWords);
+  print(totalNumberOfFoundShapes);
 }
 
 bool hasFoundKeyword(
