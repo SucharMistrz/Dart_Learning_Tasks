0d88f782f8d28b0bed7186a2c06d7bdf9362786a
Suchar <39964811+SucharMistrz@users.noreply.github.com>
2025-01-17 08:43:04 +0100

    Added comments for Day 4 Part 1

--- day4/advent_of_code_day4_part1.dart
+++ day4/advent_of_code_day4_part1.dart
@@ -1,6 +1,52 @@
 import 'dart:core';
 import 'dart:io';
 
+void main(List<String> arguments) async {
+  File inputFile = File('input.txt');
+  List<String> fileContent = await inputFile.readAsLines();
+
+  int numberOfFoundWords = 0;
+  String keyword = 'XMAS';
+
+  for (int row = 0; row < fileContent.length; row++) {
+    for (int column = 0; column < fileContent[row].length; column++) {
+      //Find the first letter of the keyword
+      if (fileContent[row][column] != keyword[0]) {
+        continue;
+      }
+
+      //Determine which directions have to be checked
+      bool goUp = false;
+      bool goDown = false;
+      bool goLeft = false;
+      bool goRight = false;
+
+      if (row >= keyword.length - 1) {
+        goUp = true;
+      }
+      if (row <= fileContent.length - keyword.length) {
+        goDown = true;
+      }
+      if (column >= keyword.length - 1) {
+        goLeft = true;
+      }
+      if (column <= fileContent[row].length - keyword.length) {
+        goRight = true;
+      }
+
+      numberOfFoundWords += findKeyword(fileContent, keyword, column, row,
+          goUp: goUp, goDown: goDown, goLeft: goLeft, goRight: goRight);
+    }
+  }
+  //This code will work with any random input
+  //but if the keyword is a palindrome, then every instance will be found twice (as normal AND backwards)!
+  if (!isPalindrome(keyword)) {
+    print(numberOfFoundWords);
+  } else {
+    print(numberOfFoundWords / 2);
+  }
+}
+
 bool isPalindrome(String word) {
   for (int i = 0; i < word.length ~/ 2; i++) {
     if (word[i] != word[word.length - i - 1]) {
@@ -99,46 +145,4 @@ int findKeyword(List<String> fileContent, String keyword, int column, int row,
   }
 
   return numberOfWordsAtThisLocation;
-}
-
-void main(List<String> arguments) async {
-  File inputFile = File('input.txt');
-  List<String> fileContent = await inputFile.readAsLines();
-
-  int numberOfFoundWords = 0;
-  String keyword = 'XMAS';
-
-  for (int row = 0; row < fileContent.length; row++) {
-    for (int column = 0; column < fileContent[row].length; column++) {
-      if (fileContent[row][column] != keyword[0]) {
-        continue;
-      }
-      bool goUp = false;
-      bool goDown = false;
-      bool goLeft = false;
-      bool goRight = false;
-
-      if (row >= keyword.length - 1) {
-        goUp = true;
-      }
-      if (row <= fileContent.length - keyword.length) {
-        goDown = true;
-      }
-      if (column >= keyword.length - 1) {
-        goLeft = true;
-      }
-      if (column <= fileContent[row].length - keyword.length) {
-        goRight = true;
-      }
-
-      numberOfFoundWords += findKeyword(fileContent, keyword, column, row,
-          goUp: goUp, goDown: goDown, goLeft: goLeft, goRight: goRight);
-    }
-  }
-
-  if (!isPalindrome(keyword)) {
-    print(numberOfFoundWords);
-  } else {
-    print(numberOfFoundWords / 2);
-  }
 }
